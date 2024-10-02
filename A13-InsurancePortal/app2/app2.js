8831ca841ffe74250c82369ee451ff7fee24befd
Kishan <kishanrajeevi.sangamone@gmail.com>
2024-10-02 13:45:53 +0400

    Made slideshow work properly

--- A13-InsurancePortal/app2/app2.js
+++ A13-InsurancePortal/app2/app2.js
@@ -2,6 +2,38 @@ let count1 = 0;
 let count2 = 0;
 let arr1 = ["text1", "text2", "text3"];
 let arr2 = ["longText1", "longText2", "longText3"]
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let img = document.getElementById("img");
+let selected = "dodgerblue";
+let notselected = "rgba(0, 0, 0, 0.3)";
+
+btn1.style.backgroundColor = selected;
+btn2.style.backgroundColor = notselected;
+btn3.style.backgroundColor = notselected;
+img.src = "../assets/img1.avif";
+
+btn1.onclick = function(){
+	btn1.style.backgroundColor = selected;
+	btn2.style.backgroundColor = notselected;
+	btn3.style.backgroundColor = notselected;
+	img.src = "../assets/img1.avif"
+}
+
+btn2.onclick = function(){
+	btn1.style.backgroundColor = notselected;
+	btn2.style.backgroundColor = selected;
+	btn3.style.backgroundColor = notselected;
+	img.src = "../assets/img2.avif"
+}
+
+btn3.onclick = function(){
+	btn1.style.backgroundColor = notselected;
+	btn2.style.backgroundColor = notselected;
+	btn3.style.backgroundColor = selected;
+	img.src = "../assets/img3.avif"
+}
 
 class DynamicText1 extends HTMLElement{
 	connectedCallback(){
