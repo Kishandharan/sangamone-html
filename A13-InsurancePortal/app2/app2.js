9c68d54412cf82a711ff0945e3b2e8b88c64a4bb
Kishandharan <kishanrajeevi.sangamone@gmail.com>
2024-10-09 16:44:20 +0400

    Added Additional Slideshows

--- A13-InsurancePortal/app2/app2.js
+++ A13-InsurancePortal/app2/app2.js
@@ -7,8 +7,12 @@ let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
 let btn4 = document.getElementById("btn4");
 let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
 let img1 = document.getElementById("img1");
 let img2 = document.getElementById("img2");
+let img3 = document.getElementById("img3");
 let selected = "dodgerblue";
 let notselected = "rgba(0, 0, 0, 0.3)";
 
@@ -17,8 +21,12 @@ btn2.style.backgroundColor = notselected;
 btn3.style.backgroundColor = notselected;
 btn4.style.backgroundColor = selected;
 btn5.style.backgroundColor = notselected;
+btn6.style.backgroundColor = selected;
+btn7.style.backgroundColor = notselected;
+btn8.style.backgroundColor = notselected;
 img1.src = "../assets/img1.avif";
 img2.src = "../assets/img4.png"
+img3.src = "../assets/img6.png"
 
 btn1.onclick = function(){
 	btn1.style.backgroundColor = selected;
@@ -51,6 +59,27 @@ btn5.onclick = function(){
 	btn4.style.backgroundColor = notselected;
 	btn5.style.backgroundColor = selected;
 	img2.src = "../assets/img5.png"
+}
+
+btn6.onclick = function(){
+	btn6.style.backgroundColor = selected;
+	btn7.style.backgroundColor = notselected;
+	btn8.style.backgroundColor = notselected;
+	img3.src = "../assets/img6.png"
+}
+
+btn7.onclick = function(){
+	btn6.style.backgroundColor = notselected;
+	btn7.style.backgroundColor = selected;
+	btn8.style.backgroundColor = notselected;
+	img3.src = "../assets/img7.png"
+}
+
+btn8.onclick = function(){
+	btn6.style.backgroundColor = notselected;
+	btn7.style.backgroundColor = notselected;
+	btn8.style.backgroundColor = selected;
+	img3.src = "../assets/img8.png"
 }
 
 
